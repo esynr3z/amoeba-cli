00e5c7c01bcd57c3a93c4b7d7b9e17edc23c828d
esynr3z <esynr3z@gmail.com>
2021-05-04 21:23:09 +0300

    Add commands for example

--- src/lib.rs
+++ src/lib.rs
@@ -1,77 +1,142 @@
-pub struct Cmd {
-    pub name: &'static str,
-    pub descr: &'static str,
-    pub args: Vec<Arg>,
+pub struct CLI {
+    pub greeting: &'static str,
+    pub prompt: &'static str,
+    pub cmds: Vec<Cmd>,
 }
 
-pub struct Arg {
-    pub name: &'static str,
-    pub descr: &'static str,
-    pub type_id: &'static str,
-}
-
-pub fn int_from_str<T>(s: &str) -> Result<T, String>
-where
-    T: num::Integer + num::Bounded + std::fmt::Display,
-{
-    let mut radix = 10;
-    let mut s_clean = s;
-    if s.starts_with("0x") {
-        radix = 16;
-        s_clean = s.trim_start_matches("0x");
-    } else if s.starts_with("0b") {
-        radix = 2;
-        s_clean = s.trim_start_matches("0b");
-    }
-    match <T>::from_str_radix(s_clean, radix) {
-        Ok(val) => Ok(val),
-        Err(_) => Err(format!(
-            "Not able to convert '{}' to integer, allowed range is [{};{}]",
-            s,
-            <T>::min_value(),
-            <T>::max_value(),
-        )),
-    }
-}
-
-pub fn float_from_str<T>(s: &str) -> Result<T, String>
-where
-    T: num::Float + std::str::FromStr,
-{
-    match s.parse::<T>() {
-        Ok(val) => Ok(val),
-        Err(_) => Err(format!("Not able to convert '{}' to float!", s)),
+impl Default for CLI {
+    fn default() -> CLI {
+        CLI {
+            greeting: "
+        @@@@@@@
+        @@@@@   @@@@@@
+      @@@@          @@@@@@@@,
+      @@@                  @@@
+     @@@@                   @@
+  (@@@@.          @@@@@    @@@
+@@@@@     @@@@@             @@@
+@@@@       @@@@@              @@@@
+@@@                @@*         @@@
+@@@@             @@@@@         @@@
+ *@@@@            @@@          @@@@
+   @@@@                     @@@@@
+    @@@     amoeba-cli     @@@@
+   ,@@@@                  @@@
+    %@@@@@@@@@@@@@@@@@@@@@@
+                   @@@@@@
+Type command and press 'Enter'. Use 'help' to list all available commands
+or 'help foobar' to get more details about specific command.
+",
+            prompt: ">",
+            cmds: Vec::new(),
+        }
     }
 }
 
-pub fn bool_from_str(s: &str) -> Result<bool, String> {
-    let true_aliases = ["true", "yes", "on", "enable", "y", "1"];
-    let false_aliases = ["false", "no", "off", "disable", "n", "0"];
-    if true_aliases.contains(&s) {
-        Ok(true)
-    } else if false_aliases.contains(&s) {
-        Ok(false)
-    } else {
-        Err(format!("Not able to convert '{}' to bool!", s))
+impl CLI {
+    fn get_cmd_by_name(&self, name: &str) -> Option<&Cmd> {
+        self.cmds.iter().filter(|x| x.name == name).next()
     }
-}
 
-pub trait CmdParser {
-    fn get_cmd_by_name(&self, name: &str) -> Option<&Cmd>;
+    fn help(&self, args: Vec<&str>) -> Result<String, String> {
+        if args.len() == 0 {
+            let mut help_str = "Available commands:\n".to_string();
+            for cmd in self.cmds.iter() {
+                help_str.push_str(&format!("  {:10} {}\n", cmd.name, cmd.descr));
+            }
+            help_str.push_str("Use 'help <command> to get more details.");
+            Ok(help_str)
+        } else {
+            let cmd_name = args[0];
+            match self.get_cmd_by_name(cmd_name) {
+                Some(cmd) => Ok(cmd.help.to_string()),
+                None => Err(format!("command '{}' was not found!", cmd_name)),
+            }
+        }
+    }
 
-    fn parse(&self, raw_str: &String) -> Result<String, String> {
+    pub fn parse(&self, raw_str: &String) -> Result<String, String> {
         // get command name and arguments from the input string
-        let mut args = raw_str.split_whitespace();
-        let cmd_name = match args.next() {
+        let mut args_iter = raw_str.split_whitespace();
+        let cmd_name = match args_iter.next() {
             Some(name) => name,
             None => return Err("Empty command name!".to_string()),
         };
-        let cmd_args: Vec<&str> = args.collect();
-        dbg!(&cmd_name, &cmd_args);
+        let cmd_args: Vec<&str> = args_iter.collect();
         // execute selected command
-        match self.get_cmd_by_name(cmd_name) {
-            Some(cmd) => return Ok(format!("command '{}' was executed!", cmd.name)),
-            None => return Err(format!("command '{}' was not found!", cmd_name)),
+        if cmd_name == "help" {
+            self.help(cmd_args)
+        } else {
+            match self.get_cmd_by_name(cmd_name) {
+                Some(cmd) => (cmd.callback)(cmd_args),
+                None => Err(format!("command '{}' was not found!", cmd_name)),
+            }
+        }
+    }
+}
+
+pub struct Cmd {
+    pub name: &'static str,
+    pub descr: &'static str,
+    pub help: &'static str,
+    pub callback: Box<dyn Fn(Vec<&str>) -> Result<String, String>>,
+}
+
+pub mod arg_utils {
+    pub fn check_len(args: &Vec<&str>, expected_len: usize) -> Result<(), String> {
+        if args.len() < expected_len {
+            Err(format!("Not enough arguments - expected {}!", expected_len))
+        } else {
+            Ok(())
+        }
+    }
+
+    pub fn int_from_str<T>(s: &str) -> Result<T, String>
+    where
+        T: num::Integer + num::Bounded + std::fmt::Display,
+    {
+        let mut radix = 10;
+        let mut s_clean = s;
+        if s.starts_with("0x") {
+            radix = 16;
+            s_clean = s.trim_start_matches("0x");
+        } else if s.starts_with("0b") {
+            radix = 2;
+            s_clean = s.trim_start_matches("0b");
+        }
+        match <T>::from_str_radix(s_clean, radix) {
+            Ok(val) => Ok(val),
+            Err(_) => Err(format!(
+                "Not able to convert '{}' to {} integer!",
+                s,
+                std::any::type_name::<T>(),
+            )),
+        }
+    }
+
+    pub fn float_from_str<T>(s: &str) -> Result<T, String>
+    where
+        T: num::Float + std::str::FromStr,
+    {
+        match s.parse::<T>() {
+            Ok(val) => Ok(val),
+            Err(_) => Err(format!(
+                "Not able to convert '{}' to {} float!",
+                s,
+                std::any::type_name::<T>()
+            )),
+        }
+    }
+
+    pub fn bool_from_str(s: &str) -> Result<bool, String> {
+        let true_aliases = ["true", "yes", "on", "enable", "y", "1"];
+        let false_aliases = ["false", "no", "off", "disable", "n", "0"];
+        if true_aliases.contains(&s) {
+            Ok(true)
+        } else if false_aliases.contains(&s) {
+            Ok(false)
+        } else {
+            Err(format!("Not able to convert '{}' to bool!", &s))
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,71 @@
-use amoeba_cli::{Arg, Cmd, CmdParser};
+use amoeba_cli::{arg_utils, Cmd, CLI};
 use std::io;
 use std::io::Write; // for flush()
 
-struct CLI {
-    cmds: Vec<Cmd>,
-}
-
-impl CmdParser for CLI {
-    fn get_cmd_by_name(&self, name: &str) -> Option<&Cmd> {
-        self.cmds.iter().filter(|x| x.name == name).next()
+fn cmd_led(args: Vec<&str>) -> Result<String, String> {
+    arg_utils::check_len(&args, 1)?;
+    let state = arg_utils::bool_from_str(args[0])?;
+    if state {
+        Ok("Led is ON now".to_string())
+    } else {
+        Ok("Led is OFF now".to_string())
     }
 }
 
-fn cmd_sum(args: &Vec<&str>) -> Result<String, String> {
-    let a = match args[0].parse::<u32>() {
-        Ok(val) => val,
-        Err(_) => return Err(format!("Argument '{}' can't be converted to u32", args[0])),
-    };
-    let b = match args[1].parse::<u32>() {
-        Ok(val) => val,
-        Err(_) => return Err(format!("Argument '{}' can't be converted to u32", args[0])),
-    };
-    Ok(format!("{}", a + b))
+fn cmd_rgb(args: Vec<&str>) -> Result<String, String> {
+    arg_utils::check_len(&args, 3)?;
+    let r = arg_utils::int_from_str::<u8>(args[0])?;
+    let g = arg_utils::int_from_str::<u8>(args[1])?;
+    let b = arg_utils::int_from_str::<u8>(args[2])?;
+    Ok(format!("Ok, R={}, G={}, B={}", r, g, b))
+}
+
+fn cmd_id(args: Vec<&str>) -> Result<String, String> {
+    arg_utils::check_len(&args, 1)?;
+    let id = args[0];
+    Ok(format!("Ok, id='{}'", id))
+}
+
+fn cmd_exit(_args: Vec<&str>) -> Result<String, String> {
+    std::process::exit(0);
 }
 
 fn main() {
     // construct comand-line interface with specific commands
     let cli = CLI {
-        cmds: vec![Cmd {
-            name: "sum",
-            descr: "sum of two arguments",
-            args: vec![
-                Arg {
-                    name: "a",
-                    descr: "argument a",
-                    type_id: "u32",
-                },
-                Arg {
-                    name: "b",
-                    descr: "argument b",
-                    type_id: "u32",
-                },
-            ],
-        }],
+        cmds: vec![
+            Cmd {
+                name: "led",
+                descr: "led control",
+                help: "Use 'led on' or 'led off' to control the state of the led.",
+                callback: Box::new(cmd_led),
+            },
+            Cmd {
+                name: "rgb",
+                descr: "RGB led control",
+                help: "rgb <red> <green> <blue>\nUse values from 0 to 255 to specify channel brightness.",
+                callback: Box::new(cmd_rgb),
+            },
+            Cmd {
+                name: "id",
+                descr: "set device id",
+                help: "id <val>\nID have to be a string value.",
+                callback: Box::new(cmd_id),
+            },
+            Cmd {
+                name: "exit",
+                descr: "exit CLI",
+                help: "Yep, no jokes, program will be terminated.",
+                callback: Box::new(cmd_exit),
+            },
+        ],
+        ..Default::default()
     };
+    print!("{}", cli.greeting);
     // imitate new string arrive (e.g. from UART)
     loop {
         let mut raw_str = String::new();
-        print!("> ");
+        print!("{}", cli.prompt);
         io::stdout().flush().unwrap(); // to ensure the output is emitted immediately
         io::stdin()
             .read_line(&mut raw_str)
